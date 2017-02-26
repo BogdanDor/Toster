154901737543f57e3fc8f18fa961669428888f53
BogdanDor <bogdandoroschenko@gmail.com>
2017-02-26 19:39:49 +0300

    modificate main page. View List questions

--- app/src/main/java/com/bogdandor/toster/MainActivity.java
+++ app/src/main/java/com/bogdandor/toster/MainActivity.java
@@ -3,9 +3,11 @@ package com.bogdandor.toster;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.bogdandor.toster.model.Loader;
+import com.bogdandor.toster.model.Question;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,24 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        new AsyncTask<Void, Void, String>() {
+        new AsyncTask<Void, Void, Question[]>() {
             Exception exception = null;
 
-            protected String doInBackground(Void... params) {
-                String text = null;
+            protected Question[] doInBackground(Void... params) {
+                Question[] questions = null;
                 try {
-                    text = Loader.getInstance().getQuestions()[0].title;
+                    questions = Loader.getInstance().getQuestions();
                 } catch (Exception e) {
                     exception = e;
                 }
-                return text;
+                return questions;
             }
 
-            protected void onPostExecute(String text) {
+            protected void onPostExecute(Question[] questions) {
                 if (exception == null) {
                     setContentView(R.layout.activity_main);
-                    final TextView myTextView = (TextView) findViewById(R.id.myTextView);
-                    myTextView.setText(text);
+                    ListView listQuestions = (ListView) findViewById(R.id.list_questions);
+                    ArrayAdapter<Question> listAdapter = new ArrayAdapter<Question>(
+                            MainActivity.this,
+                            android.R.layout.simple_list_item_1,
+                            questions);
+                    listQuestions.setAdapter(listAdapter);
                 } else {
                     setContentView(R.layout.error);
                 }
--- app/src/main/java/com/bogdandor/toster/model/Question.java
+++ app/src/main/java/com/bogdandor/toster/model/Question.java
@@ -2,4 +2,7 @@ package com.bogdandor.toster.model;
 
 public class Question {
     public String title;
+    public String toString() {
+        return title;
+    }
 }
