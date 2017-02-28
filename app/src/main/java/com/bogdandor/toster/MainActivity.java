a68b47adafe197276e385e9179637272deea8dd2
BogdanDor <bogdandoroschenko@gmail.com>
2017-02-28 22:25:07 +0300

    add pages and navigation buttons

--- app/src/main/java/com/bogdandor/toster/MainActivity.java
+++ app/src/main/java/com/bogdandor/toster/MainActivity.java
@@ -3,44 +3,92 @@ package com.bogdandor.toster;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
-import com.bogdandor.toster.model.Loader;
+import com.bogdandor.toster.model.PageQuestions;
 import com.bogdandor.toster.model.Question;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+    ListView listQuestions;
+    Button prev;
+    String prevPage;
+    Button next;
+    String nextPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listQuestions = (ListView) findViewById(R.id.list_questions);
+        prev = (Button) findViewById(R.id.prev);
+        next = (Button) findViewById(R.id.next);
 
-        new AsyncTask<Void, Void, Question[]>() {
-            Exception exception = null;
+        new DownloaderPage().execute();
+    }
 
-            protected Question[] doInBackground(Void... params) {
-                Question[] questions = null;
-                try {
-                    questions = Loader.getInstance().getQuestions();
-                } catch (Exception e) {
-                    exception = e;
+    public void onClickPrev(View view) {
+        new DownloaderPage().execute(prevPage);
+    }
+
+    public void onClickNext(View view) {
+        new DownloaderPage().execute(nextPage);
+    }
+
+    void viewPageQuestions(PageQuestions pageQuestions) throws IOException {
+        Question[] questions = pageQuestions.getQuestions();
+        ArrayAdapter<Question> listAdapter = new ArrayAdapter<Question>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                questions);
+        listQuestions.setAdapter(listAdapter);
+
+        prevPage = pageQuestions.getPrevPage();
+        prev.setVisibility(View.VISIBLE);
+        if (prevPage == null) {
+            prev.setVisibility(View.INVISIBLE);
+        }
+        nextPage = pageQuestions.getNextPage();
+        next.setVisibility(View.VISIBLE);
+        if (nextPage == null) {
+            next.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
+    private class DownloaderPage extends AsyncTask<String, Void, PageQuestions> {
+        Exception exception = null;
+
+        protected PageQuestions doInBackground(String... params) {
+            PageQuestions pageQuestions = null;
+            try {
+                if (params.length == 0) {
+                    pageQuestions = new PageQuestions();
+                } else {
+                    String url = params[0];
+                    pageQuestions = new PageQuestions(url);
                 }
-                return questions;
+            } catch (Exception e) {
+                exception = e;
             }
+            return pageQuestions;
+        }
 
-            protected void onPostExecute(Question[] questions) {
+        protected void onPostExecute(PageQuestions pageQuestions) {
+            try {
                 if (exception == null) {
-                    setContentView(R.layout.activity_main);
-                    ListView listQuestions = (ListView) findViewById(R.id.list_questions);
-                    ArrayAdapter<Question> listAdapter = new ArrayAdapter<Question>(
-                            MainActivity.this,
-                            android.R.layout.simple_list_item_1,
-                            questions);
-                    listQuestions.setAdapter(listAdapter);
+                    viewPageQuestions(pageQuestions);
                 } else {
                     setContentView(R.layout.error);
                 }
+            } catch (Exception e) {
+                setContentView(R.layout.error);
             }
-        }.execute();
+        }
+
     }
 }
---
+++ app/src/main/java/com/bogdandor/toster/model/PageQuestions.java
@@ -0,0 +1,53 @@
+package com.bogdandor.toster.model;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class PageQuestions {
+    Document doc;
+
+    public PageQuestions(String siteUrl) throws IOException {
+        doc = Jsoup.connect(siteUrl).get();
+    }
+
+    public PageQuestions() throws IOException {
+        this("https://www.toster.ru");
+    }
+
+    public Question[] getQuestions() throws IOException {
+        Elements elements = doc.select("h2.question__title");
+        Question[] questions = new Question[elements.size()];
+        for (int i=0; i<questions.length; i++) {
+            questions[i] = new Question();
+        }
+        for (int i=0; i<elements.size(); i++) {
+            questions[i].title = elements.get(i).text();
+        }
+        return questions;
+    }
+
+    public String getNextPage() throws IOException {
+        Elements elements = doc.select(".paginator__item.next");
+        if (!elements.isEmpty()) {
+            Element element = elements.first();
+            String link = element.select("a[href]").first().attr("href");
+            return link;
+        }
+        return null;
+    }
+
+    public String getPrevPage() throws IOException {
+        Elements elements = doc.select(".paginator__item.prev");
+        if (!elements.isEmpty()) {
+            Element element = elements.first();
+            String link = element.select("a[href]").first().attr("href");
+            return link;
+        }
+        return null;
+    }
+
+}
